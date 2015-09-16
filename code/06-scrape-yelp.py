4df69c5fde957043228cd63fcb203ea5cf3cf976
Dan Wilhelm <dan@danwilhelm.com>
2015-09-15 22:46:11 -0700

    Shows example scraped tags in comments

--- code/06-scrape-yelp.py
+++ code/06-scrape-yelp.py
@@ -3,22 +3,30 @@ from bs4 import BeautifulSoup
 
 
 YELP_URL = 'http://www.yelp.com/biz/wills-jamaican-cuisine-inglewood?hrid=lFvP6CD1LgMYInElXwpsEQ'
-GOOGLE_URL = 'https://news.google.com'
 
 r = requests.get(YELP_URL)
 bs = BeautifulSoup(r.text)
 
+# <h1 class="biz-page-title">Business Name</h1>
 name = bs.select('h1.biz-page-title')[0].text.strip()
+
+# <address>1253 2nd St; Santa Monica, CA</address>
 address = bs.select('address')[0].text.strip()
+
+# <p itemprop="description">Review Here</p>
 reviews = [review.text for review in bs.select('p[itemprop="description"]')]
 
+# <meta itemprop="ratingValue content="3.0" />
 rating_tags = bs.select('meta[itemprop="ratingValue"]')
-ratings = [float(tag.attrs['content']) for tag in rating_tags]
+ratings = [float(tag.attrs['content']) for tag in rating_tags]   
 
+# <span class="hour-range"><span>8:30am</span> - <span>5:00pm</span></span>
 hours_open_today = bs.select('span.hour-range')[0].get_text()
 
+# <div class="short-def-list"><dl> ... </dl><dl> ... </dl></div>
 biz_info_tags = bs.select('div.short-def-list > dl')
 
+# <dl> <dt>key</dt>  <dd>value</dd> </dl>
 biz_info = {}
 for tag in biz_info_tags:
 	key = tag.select('dt')[0].text.strip()
